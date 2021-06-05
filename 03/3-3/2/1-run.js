a31e7fc2fc0261be71b437e79853303e27aebef2
平文 <pwstrick@163.com>
2021-06-05 14:35:04 +0800

    第三章代码

--- 03/3-3/2/1-run.js
+++ 03/3-3/2/1-run.js
@@ -1,8 +1,7 @@
 import { name, setName } from "./1.js";
-name = "freedom";	//错误
-// console.log(name);	//"strick"
-// setName("freedom");
-// console.log(name);	//"freedom"
+console.log(name);	//"strick"
+setName("freedom");
+console.log(name);	//"freedom"
 
 
 
